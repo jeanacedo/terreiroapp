70f7518031cd60821ff62f8c1fa63e4fefefb0e1
jacedos <jean.acedo@gmail.com>
2023-06-26 22:56:10 -0300

    classe de criação para registro dos filhos

--- juma_sis/forms.py
+++ juma_sis/forms.py
@@ -3,30 +3,30 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email
 
 class FormLogin(FlaskForm):
-    username = StringField('Email', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired()])
+    username = StringField('Email', validators=[DataRequired(message='Campo obrigatório'), Email(message='Digite um email válido')])
+    senha = PasswordField('Senha', validators=[DataRequired(message='Campo obrigatório')])
     remember = BooleanField('Lembrar acesso')
     button_submit = SubmitField('Login')
 
 class RegistroFilho(FlaskForm):
-    # matricula =
-    # nome =
-    # nascimento =
-    # inicio =
-    # status =
-    # hierarquia =
-    # orixa1 =
-    # orixa2 =
-    # orixa3 =
-    # telefone =
-    # email =
-    # rg =
-    # cpf =
-    # rua =
-    # ncasa =
-    # complemento =
-    # bairro =
-    # cidade =
-    # uf =
-    # idresp =
-    # img_filho =
+    nome = StringField('Nome completo')
+    nascimento_dia = StringField('Dia')
+    nascimento_mes = StringField('Mês')
+    nascimento_ano = StringField('Ano')
+    inicio_dia = StringField('Dia')
+    inicio_mes = StringField('Mês')
+    inicio_ano = StringField('Ano')
+    orixa1 = StringField('Orixá 1')
+    orixa2 = StringField('Orixá 2')
+    orixa3 = StringField('Orixá 3')
+    telefone = StringField('Telefone')
+    email = StringField('Email', validators=[Email(message='Email Inválido')])
+    rg = StringField('RG')
+    cpf = StringField('CPF', validators=[DataRequired(message='Campo obrigatório'), Length(min=11, max=11)])
+    rua = StringField('Rua')
+    ncasa = StringField('Número')
+    complemento = StringField('Complemento')
+    bairro = StringField('Bairro')
+    cidade = StringField('Cidade')
+    uf = StringField('UF')
+    idresp = StringField('responsável(se menor)')
